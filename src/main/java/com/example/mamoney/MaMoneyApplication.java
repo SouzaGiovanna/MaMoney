732a50fccd50e9db2d38c8c3265c2c1a4770bb15
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2023-03-20 18:59:38 -0300

    initial commit

---
+++ src/main/java/com/example/mamoney/MaMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaMoneyApplication.class, args);
+    }
+
+}
